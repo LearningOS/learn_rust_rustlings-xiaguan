f72a3fa9e1a5942efbd897cab50527e8c9c9236b
xiaguan <751080330@qq.com>
2022-10-31 16:27:46 +0800

    pass quiz1

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,10 @@
 // functions1.rs
-// Make me compile! Execute `rustlings hint functions1` for hints :)
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
+fn call_me(){
+    println!("call the func");
+}
 fn main() {
     call_me();
 }
